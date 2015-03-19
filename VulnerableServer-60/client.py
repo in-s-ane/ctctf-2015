b943ee03d4f261d1dea3e0ae79d965d3cfcb21ac
elc1798 <subliminalmau5@gmail.com>
2015-03-18 23:25:42 -0400

    Cleanup

--- VulnerableServer-60/client.py
+++ VulnerableServer-60/client.py
@@ -1,22 +1,17 @@
 import urllib2
 
 HOST = "http://vulnserver-failedxyz.c9.io/login.php"
-PORT = 80
 
 def con(username , password , char , answer):
     # sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     TMP = HOST + "?username=" + username + "&password=" + password
     TMP = TMP.replace(" " , "%20")
-    # print("Query: " + TMP)
-    # sock.connect((TMP, PORT))
-    # received = sock.recv(1024)
     request = urllib2.Request(TMP , "")
     received = urllib2.urlopen(request).read()
     # print received
     if "You're in!" in received:
         answer += char
     return answer
-    # sock.close()
 
 alpha = "abcdefhijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_"
 
